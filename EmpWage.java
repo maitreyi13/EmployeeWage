5ff3e5fc6a743ba43e1e8bedfb262d013ae2793b
Maitreyi Kulkarni <107098811+maitreyi13@users.noreply.github.com>
2022-07-05 12:33:32 +0530

    Compute Employee Wage for multiple companies
    Each Company has its own wage, number of working days and working hours per month

--- EmpWage.java
+++ EmpWage.java
@@ -1,8 +1,21 @@
 public class EmpWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Problem::");
-        EmployeeWage emp = new EmployeeWage();
-        emp.EmpWage();
+        System.out.println("Welcome to Employee Wage Problem ");
+        System.out.println("**************************************");
+        System.out.println("\n Infosys Employee Data \n ");
+        EmployeeWage Infy = new EmployeeWage();
+        Infy.EmpWage(20,8,4,20,100);
+        System.out.println("**********************************************\n");
+
+        System.out.println("\nWipro Employee Data\n");
+        EmployeeWage Wipro = new EmployeeWage();
+        Wipro.EmpWage(25,10,5,28,140);
+        System.out.println("*********************************************** \n");
+
+        System.out.println("\n TCS Employee Data ");
+        EmployeeWage TCS = new EmployeeWage();
+        TCS.EmpWage(28,12,6,25,180);
+        System.out.println("********************************************");
 
     }
 }
