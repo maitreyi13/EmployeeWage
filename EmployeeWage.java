4a5547978b99fe34e0305103dc1bd68cd180b760
Maitreyi Kulkarni <107098811+maitreyi13@users.noreply.github.com>
2022-07-05 10:32:17 +0530

    Add Part time Employee & Wage
    Assume Part time Hour is 4

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,17 +1,20 @@
 import java.lang.*;
 public class EmployeeWage {
-    public static void main(String[] args) {
-        int attfull = 1;
-        int  wph = 20;
-        int emphrs ,empwage;
-        double a = Math.floor(Math.random() * 10) % 2;
-        if (a == attfull)
-            emphrs = 8;
-        else
-            emphrs = 0;
 
-        empwage = emphrs * wph;
-        System.out.println("Daily wage for full time employee : "+empwage);
-    }
+        public static void main(String[] args) {
+            int attpart = 1;
+            int attfull = 2;
+            int wph = 20;
+            int emphrs ,empwage ;
+            double a = Math.floor(Math.random() * 10) % 3;
+            if (a == attpart)
+                emphrs = 4;
+            else if (a == attfull)
+                emphrs = 8;
+            else
+                emphrs = 0;
 
+            empwage = emphrs * wph;
+            System.out.println("Daily wage for an employee : "+empwage);
+        }
 }
