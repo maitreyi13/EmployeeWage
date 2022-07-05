7404bfb5acca15798ad53f4415fc1aecfbf90dd6
Maitreyi Kulkarni <107098811+maitreyi13@users.noreply.github.com>
2022-07-05 10:39:57 +0530

    Calculating Wages for a Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,15 +3,20 @@ public class EmployeeWage {
         public static final int attpart = 1;
         public static final int attfull = 2;
         public static final int wph = 20;
+        public static final int wdm = 20;
         public static void main(String[] args) {
-            int emphrs ,empwage;
-            int a = (int) Math.floor(Math.random() * 10) % 3;
-            emphrs = switch (a) {
-                case attfull -> 8;
-                case attpart -> 4;
-                default -> 0;
-            };
-            empwage = emphrs * wph;
-            System.out.println("Daily wage for an employee : "+empwage);
+            int emphrs ,empwage; int totalempwage = 0;
+            for (int days = 1; days<=wdm; days++) {
+                int a = (int) Math.floor(Math.random() * 10) % 3;
+                emphrs = switch (a) {
+                    case attfull -> 8;
+                    case attpart -> 4;
+                    default -> 0;
+                };
+                empwage = emphrs * wph;
+                totalempwage += empwage;
+                System.out.println("Daily wage for an employee : " + empwage);
+            }
+            System.out.println("Monthly wage for an employee : " + totalempwage);
         }
 }
