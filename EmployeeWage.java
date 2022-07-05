df7d2ca653598b83ef4721ca6bddcf87473321d4
Maitreyi Kulkarni <107098811+maitreyi13@users.noreply.github.com>
2022-07-05 10:36:20 +0530

    Solving using Switch Case Statement

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,19 +1,16 @@
 import java.lang.*;
 public class EmployeeWage {
-
+        public static final int attpart = 1;
+        public static final int attfull = 2;
+        public static final int wph = 20;
         public static void main(String[] args) {
-            int attpart = 1;
-            int attfull = 2;
-            int wph = 20;
-            int emphrs ,empwage ;
-            double a = Math.floor(Math.random() * 10) % 3;
-            if (a == attpart)
-                emphrs = 4;
-            else if (a == attfull)
-                emphrs = 8;
-            else
-                emphrs = 0;
-
+            int emphrs ,empwage;
+            int a = (int) Math.floor(Math.random() * 10) % 3;
+            emphrs = switch (a) {
+                case attfull -> 8;
+                case attpart -> 4;
+                default -> 0;
+            };
             empwage = emphrs * wph;
             System.out.println("Daily wage for an employee : "+empwage);
         }
